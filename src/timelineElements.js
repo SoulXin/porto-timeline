50ec473d2b56f60086ec96958940501346262414
Denny Hendrawan <dennyhendrawan@gmail.com>
2022-08-02 08:54:15 +0700

    First init

---
+++ src/timelineElements.js
@@ -0,0 +1,70 @@
+let timelineElements = [
+  {
+    id: 1,
+    title: "Mobile Developer",
+    location: "Freelance",
+    description: [
+      "Create some project for client",
+      "Maintaince code",
+      "Fix bug and error in their project before publish"
+    ],
+    date: "April 2022 - present",
+    icon: "work",
+  },
+  {
+    id: 2,
+    title: "Fullstack Developer",
+    location: "PT. Informasi Guna Optima",
+    description: [
+      "Maintaince existing program",
+      "Fix bug or error in program",
+      "Setting server amazon EC2",
+      "Test program in development before go to production",
+      "Add new module in program"
+    ],
+    buttonText: "Company Website",
+    website: "https://ingo.io/",
+    date: "Juli 2021 - Present",
+    icon: "work",
+  },
+  {
+    id: 3,
+    title: "IT Support",
+    location: "CV. Armada Integrasi Teknologi",
+    description:[
+      "Follow up client",
+      "Help programmer find bug",
+      "Handle complaint client about error program"
+    ],
+    buttonText: "Company Website",
+    website: "http://wp.armada-it.com/",
+    date: "April 2021 - Juni 2021",
+    icon: "work",
+  },
+  {
+    id: 4,
+    title: "Web Developer",
+    location: "Inti Grafika",
+    description:[
+      "Make admin web system for company",
+      "Maintaince existing program"
+    ],
+    buttonText: "Company Website",
+    website: "https://inti-grafika.com/",
+    date: "February 2021 - March 2021",
+    icon: "work",
+  },
+  {
+    id: 5,
+    title: "Computer Laboratory Assistant",
+    location: "Mikroskil University",
+    description: [
+      "Help lecturers when teaching in class",
+      "Fix bugs or errors in a student program when the learning process is in progress"
+    ],
+    date: "September 2018 - April 2020",
+    icon: "work",
+  }
+];
+
+export default timelineElements;
